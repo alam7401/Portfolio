e0db4acb76cee0caa4eb71811b6cfbf4c0312c55
Tauqeer Alam <tauqeeralam7401@gmail.com>
2026-09-22 00:18:44 +0530

    Configure Render SQLite deployment

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get(
     "django-insecure-local-dev-key-only",
 )
 
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = os.environ.get("DEBUG", "False") == "False"
 
 ALLOWED_HOSTS = [
     ".onrender.com",
